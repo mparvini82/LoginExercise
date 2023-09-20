fcac21b6c7e77e7ba0aa35699cb7175d6c6d38f9
Bagheri <4hm4db4gheri@gmail.com>
2023-09-20 15:09:10 +0330

    Create Signout btn made side bar prettier

--- Site1.Master.designer.cs
+++ Site1.Master.designer.cs
@@ -78,6 +78,15 @@ namespace LoginExercise
         protected global::System.Web.UI.HtmlControls.HtmlAnchor nav4;
 
         /// <summary>
+        /// SignOutBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SignOutBtn;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
